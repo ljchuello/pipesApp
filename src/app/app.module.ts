390df3c3db68727ae45a420314c135e6b5486b38
Leonardo Chuello <ljchuello@gmail.com>
2021-06-19 15:55:18 -0500

    142

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,18 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
-import localEsEc from '@angular/common/locales/es-EC';
+import localEs from '@angular/common/locales/es';
+import localIt from '@angular/common/locales/it';
 import { registerLocaleData } from '@angular/common';
-
-registerLocaleData(localEsEc);
+registerLocaleData(localEs);
+registerLocaleData(localIt);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: LOCALE_ID, useValue: 'es-EC' },
+    { provide: LOCALE_ID, useValue: 'es' },
   ],
   bootstrap: [AppComponent],
 })
