201dda135a02615ac4f18c624a1f77a9fd00e3fa
Leonardo Chuello <ljchuello@gmail.com>
2021-06-19 17:20:16 -0500

    147

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -19,4 +19,12 @@ export class NoComunesComponent implements OnInit {
   ngOnInit(): void {
     this.primengConfig.ripple = true;
   }
+
+  clientes: string[] = ['Leonardo', 'Maria', 'Paola', 'Belen'];
+
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente en esperando',
+    '=1': 'tenemos un ningun cliente en esperando',
+    other: 'tenemos # clientes esperando',
+  };
 }
