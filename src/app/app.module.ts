9327f90503188113ffcffce57086bbbffdbcff42
Leonardo Chuello <ljchuello@gmail.com>
2021-06-19 15:02:07 -0500

    138

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
