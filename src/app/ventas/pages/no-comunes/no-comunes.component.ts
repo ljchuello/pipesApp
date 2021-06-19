8869a009252ead156478d034ad27af25217ca56a
Leonardo Chuello <ljchuello@gmail.com>
2021-06-19 17:32:51 -0500

    148

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,11 +8,21 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class NoComunesComponent implements OnInit {
   nombre: string = 'Leonardo';
+
   genero: string = 'masculino';
+
   invitacionObj = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
   };
+
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente en esperando',
+    '=1': 'tenemos un cliente en esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  clientes: string[] = ['Leonardo', 'Maria', 'Paola', 'Belen'];
 
   constructor(private primengConfig: PrimeNGConfig) {}
 
@@ -20,11 +30,17 @@ export class NoComunesComponent implements OnInit {
     this.primengConfig.ripple = true;
   }
 
-  clientes: string[] = ['Leonardo', 'Maria', 'Paola', 'Belen'];
+  cambiarPersona() {
+    if (this.genero === 'masculino') {
+      this.nombre = 'Paola Vizcano';
+      this.genero = 'femenino';
+    } else {
+      this.nombre = 'Leonardo Chuello';
+      this.genero = 'masculino';
+    }
+  }
 
-  clientesMap = {
-    '=0': 'no tenemos ningun cliente en esperando',
-    '=1': 'tenemos un ningun cliente en esperando',
-    other: 'tenemos # clientes esperando',
-  };
+  eliminarPersona() {
+    this.clientes = this.clientes.slice(1);
+  }
 }
