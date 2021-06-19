18e3c457e397dee14566839847030cd7f6787203
Leonardo Chuello <ljchuello@gmail.com>
2021-06-19 13:31:08 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'leonardo jesus';
+  valor: number = 1000;
+
+  obj = {
+    nombre: 'Leonardo',
+  };
+
+  cambiarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
