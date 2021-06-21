1ebe9897702de6e6f85c6be23024dcd54cbe2029
Leonardo Chuello <ljchuello@gmail.com>
2021-06-20 21:23:56 -0500

    159

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRouterModule } from './app-router.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRouterModule} from './app-router.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { VentasModule } from './ventas/ventas.module';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {VentasModule} from './ventas/ventas.module';
 
 import localEs from '@angular/common/locales/es';
 import localIt from '@angular/common/locales/it';
-import { registerLocaleData } from '@angular/common';
+import {registerLocaleData} from '@angular/common';
+
 registerLocaleData(localEs);
 registerLocaleData(localIt);
 
@@ -24,9 +25,10 @@ registerLocaleData(localIt);
     BrowserAnimationsModule,
   ],
   providers: [
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: LOCALE_ID, useValue: 'es' },
+    {provide: LocationStrategy, useClass: HashLocationStrategy},
+    {provide: LOCALE_ID, useValue: 'es'},
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
